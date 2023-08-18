52e9426dd02fee98712df44e8c2c2366d6bcca61
Caiobaldur <caiobrafachino@hotmail.com>
2023-08-17 23:15:55 -0300

    add-middleware-function-verify-admin

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,17 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+// function myMiddleware(request, response, next) {
+  
+//   if(!request.body.isAdmin){
+//     return response.json({message: "Usuário não autorizado!"});
+//   }
+  
+//   next();
+// }
+
 const usersController = new UsersController();
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/",usersController.create);
 
 module.exports = usersRoutes;
