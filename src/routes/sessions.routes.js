0e3226bb80270612626b6d083ed44393113a1ec8
Caio Balduino <caiobrafachino@hotmail.com>
2023-10-11 19:42:49 -0300

    correção rota

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const SessionsController = require("../controllers/SessionsController");
 const sessionsController = new SessionsController();
 
-const sessionsController = Router();
+const sessionsRoutes = Router();
 sessionsRoutes.post("/", sessionsController.create);
 
 module.exports = routes;
