5fe8e7a5b1afd3941151ded6e0f94c93341a2841
Caio Balduino <caiobrafachino@hotmail.com>
2023-10-11 19:49:53 -0300

    Validação de usuário

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,15 @@
-class SessionsController {
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
-  async create(request, response){
-    const { email, password } = request.body;
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body
+
+    const user = await knex("users").where({ email }).first();
+
+    if(!user) {
+      throw new AppError("E-mail e/ou senha incorreta!", 401);
+    }
 
     return response.json({ email, password })
   }
