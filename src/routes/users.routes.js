c1f9f2952c16465e3358fa3909257ede55d47504
Caio Balduino <caiobrafachino@hotmail.com>
2023-10-16 19:25:17 -0300

    Criação de Middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,21 +1,13 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
 const usersRoutes = Router();
-
-// function myMiddleware(request, response, next) {
-  
-//   if(!request.body.isAdmin){
-//     return response.json({message: "Usuário não autorizado!"});
-//   }
-  
-//   next();
-// }
 
 const usersController = new UsersController();
 
 usersRoutes.post("/",usersController.create);
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = usersRoutes;
